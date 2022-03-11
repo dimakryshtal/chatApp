d206e54f1cb476669956f72140c96acb3f16003d
dimakryshtal <dima.krshl@gmail.com>
2022-03-11 21:42:46 +0200

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Dima Kryshtal on 11.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
